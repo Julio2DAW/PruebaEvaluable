ee751ffbafc08db36457c94674775cdaece65f34
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-13 10:38:38 +0200

    sub2

--- PRU-01.02-master/ejercicio2/js/ejercicio2.js
+++ PRU-01.02-master/ejercicio2/js/ejercicio2.js
@@ -9,3 +9,19 @@
 */
 
 'use strict'
+
+function comprobarEdad(fechaNacimiento){
+  let anio=new Date().getFullYear();
+  let mes=new Date().getMonth()+1;
+  let edad=anio-fechaNacimiento.substr(0,4);
+  //Saber los años que tiene para comprobar si es mayor de edad
+  if(fechaNacimiento.substr(5,2)<mes){
+    edad--;
+    //Saber si es mayor de edad o menor y mostrar el mensaje por consola
+    if(edad>=18){
+      console.log("Mayor de edad");
+    }else{
+      console.log("Menor de edad");
+    }
+  }
+}
