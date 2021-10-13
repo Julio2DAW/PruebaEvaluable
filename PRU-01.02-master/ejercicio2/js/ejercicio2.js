b0c5ce72079878500f2400263795196472f1c5c1
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-13 09:58:53 +0200

    sub1

---
+++ PRU-01.02-master/ejercicio2/js/ejercicio2.js
@@ -0,0 +1,11 @@
+/*
+  Ejercicio 2
+  Autor: Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
+  Licencia: GPL v3 o superior.
+  Año 2021
+  Programa la función comprobarEdad para comprobar si la fecha introducida en el campo de fecha
+  corresponde a alguien mayor de 18 años o no.
+  La función mostrará un mensaje por consola indicando si es mayor de edad o no.
+*/
+
+'use strict'
